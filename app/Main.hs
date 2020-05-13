10d78bc342bf3313e567b981b9a69e561e15afb4
kita127 <kitada.kosuke@gmail.com>
2020-05-14 08:50:52 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,15 @@ module Main where
 
 import qualified Language.C                    as C
 import qualified Language.C.System.GCC         as GCC
+import qualified Language.C.Pretty             as Pretty
 
 main :: IO ()
 main = do
     res <- C.parseCFile (GCC.newGCC "gcc")
                         Nothing
                         ["-I/usr/include/gtk-2.0"]
-                        "./examples/sample1.c"
+                        "./examples/sample0.c"
+    putStrLn ""
     case res of
         Right a -> print a
         _       -> putStrLn "parse error"
