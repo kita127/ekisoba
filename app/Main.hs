b2987e7ee9b3fa19b92a2b17a6e6d5ac890c2c51
kita127 <kitada.kosuke@gmail.com>
2020-05-15 13:52:16 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,19 @@
 module Main where
 
-import qualified Language.C                    as C
-import qualified Language.C.System.GCC         as GCC
-import qualified Language.C.Pretty             as Pretty
+import qualified Language.C            as C
+import qualified Language.C.Ekisoba    as Eki
+import qualified Language.C.Pretty     as Pretty
+import qualified Language.C.System.GCC as GCC
 
 main :: IO ()
 main = do
-    res <- C.parseCFile (GCC.newGCC "gcc")
+    parseRes <- C.parseCFile (GCC.newGCC "gcc")
                         Nothing
                         ["-I/usr/include/gtk-2.0"]
                         "./examples/sample0.c"
     putStrLn ""
-    case res of
-        Right a -> print a
-        _       -> putStrLn "parse error"
+    let transRes = case parseRes of
+            Right a -> Eki.translate a
+            _       -> error "parse error"
+
+    print transRes
--- src/Language/C/Ekisoba.hs
+++ src/Language/C/Ekisoba.hs
@@ -1,1 +1,6 @@
-module Language.C.Ekisoba () where
+module Language.C.Ekisoba ( translate ) where
+
+import qualified Language.C.Syntax.AST as AST
+
+translate :: AST.CTranslUnit -> String
+translate _ = "result"
