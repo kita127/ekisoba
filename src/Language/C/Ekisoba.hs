265be9eaa6c1c17418c8928238768f96f620f83d
kita127 <kitada.kosuke@gmail.com>
2020-05-15 13:54:03 +0900

    update

--- src/Language/C/Ekisoba.hs
+++ src/Language/C/Ekisoba.hs
@@ -3,4 +3,4 @@ module Language.C.Ekisoba ( translate ) where
 import qualified Language.C.Syntax.AST as AST
 
 translate :: AST.CTranslUnit -> String
-translate _ = "result"
+translate ast = show ast
