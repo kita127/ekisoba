ac2e98b4529a4f9d20980d06ff5a07db91023f65
kita127 <kitada.kosuke@gmail.com>
2020-05-15 17:34:51 +0900

    format

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import qualified Language.C            as C
-import qualified Language.C.Ekisoba    as Eki
-import qualified Language.C.Pretty     as Pretty
-import qualified Language.C.System.GCC as GCC
-import qualified Data.Aeson    as Aes
-import qualified Data.Aeson.TH as TH
-import qualified Data.Aeson.Encode.Pretty as AesP
-import qualified Data.ByteString.Lazy    as B
+import qualified Data.Aeson                 as Aes
+import qualified Data.Aeson.Encode.Pretty   as AesP
+import qualified Data.Aeson.TH              as TH
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Language.C                 as C
+import qualified Language.C.Ekisoba         as Eki
+import qualified Language.C.Pretty          as Pretty
+import qualified Language.C.System.GCC      as GCC
 
 main :: IO ()
 main = do
